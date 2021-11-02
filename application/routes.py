b2c35b4c0be80eb6faaa513c03a42c34aefb19bc
WANI0N <marek.beck2@gmail.com>
2021-11-02 15:14:02 +0200

    reactivated force https, footer corrected spelling

--- application/routes.py
+++ application/routes.py
@@ -193,12 +193,12 @@ class SubmitAlerts(Resource):
 #################################
 
 # Force https, does not work for local host
-# @app.before_request
-# def before_request():
-#     if not request.is_secure:
-#         url = request.url.replace('http://', 'https://', 1)
-#         code = 301
-#         return redirect(url, code=code)
+@app.before_request
+def before_request():
+    if not request.is_secure:
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
 
 ##ROUTES
 ##NAVIGATIONAL
